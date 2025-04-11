0b8d0c930ff981af6ed1dc569516691a1f13e8f8
unknown <riyakothari157@gmail.com>
2025-04-12 00:41:30 +0530

    Initial commit

---
+++ home-services/src/main/java/com/homeservices/home_services/HomeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.homeservices.home_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServicesApplication.class, args);
+	}
+
+}
---
+++ home-services/src/test/java/com/homeservices/home_services/HomeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeservices.home_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
